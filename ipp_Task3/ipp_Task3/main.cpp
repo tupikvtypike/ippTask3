b3742817376c232058dfe13d6532e99a5e7533bc
tupikvtypike <tupikvtypike@gmail.com>
2017-10-17 18:05:04 +0300

    Добавлен подсчет ускорения. Исправления в параллелизацию.

--- ipp_Task3/ipp_Task3/main.cpp
+++ ipp_Task3/ipp_Task3/main.cpp
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <ctime>
+#include <stdio.h>
+#include <windows.h>
+#include <mmsystem.h>
 #include <cilk/cilk.h>
 #include <cilk/reducer_opadd.h>
-#include <chrono>
 
 #include <iostream>
-
-using namespace std::chrono;
 
 // количество строк в исходной квадратной матрице
 const int MATRIX_SIZE = 1500;
@@ -34,13 +34,13 @@ void IntiMatrix(double** matrix)
 /// последний столбей матрицы - значения правых частей уравнений
 /// rows - количество строк в исходной квадратной матрице
 /// result - массив ответов СЛАУ
-void SerialGaussMethod(double **matrix, const int rows, double* result)
+int SerialGaussMethod(double **matrix, const int rows, double* result)
 {
 	int k;
 	double koef;
 
-	std::chrono::time_point<std::chrono::system_clock> start, end;
-	start = std::chrono::system_clock::now();
+	DWORD starttime, elapsedtime;
+	starttime = timeGetTime();
 
 	// прямой ход метода Гаусса
 	for (k = 0; k < rows; ++k)
@@ -54,9 +54,8 @@ void SerialGaussMethod(double **matrix, const int rows, double* result)
 		}
 	}
 
-	end = std::chrono::system_clock::now();
-	int elapsed_seconds = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-	std::cout << "Время работы прямого хода: " << elapsed_seconds << " микросекунд.\n";
+	elapsedtime = timeGetTime() - starttime;
+	std::cout << "Время работы прямого хода: " << elapsedtime << " миллисекундах.\n";
 
 	// обратный ход метода Гаусса
 	result[rows - 1] = matrix[rows - 1][rows] / matrix[rows - 1][rows - 1];
@@ -70,6 +69,7 @@ void SerialGaussMethod(double **matrix, const int rows, double* result)
 		}
 		result[k] /= matrix[k][k];
 	}
+	return elapsedtime;
 }
 
 /// Функция SerialGaussParallelMethod() решает СЛАУ методом Гаусса 
@@ -77,70 +77,90 @@ void SerialGaussMethod(double **matrix, const int rows, double* result)
 /// последний столбей матрицы - значения правых частей уравнений
 /// rows - количество строк в исходной квадратной матрице
 /// result - массив ответов СЛАУ
-void SerialGaussParallelMethod(double **matrix, const int rows, double* result)
+int SerialGaussParallelMethod(double **matrix, const int rows, double* result)
 {
-	int k;
-	double koef;
-
-	std::chrono::time_point<std::chrono::system_clock> start, end;
-	start = std::chrono::system_clock::now();
+	DWORD starttime, elapsedtime;
+	starttime = timeGetTime();
 
 	// прямой ход метода Гаусса
-	for (k = 0; k < rows; ++k)
+	for (int k = 0; k < rows; ++k)
 	{
 		//
 		cilk_for(int i = k + 1; i < rows; ++i)
 		{
-			koef = -matrix[i][k] / matrix[k][k];
+			double koef = -matrix[i][k] / matrix[k][k];
 			for (int j = k; j <= rows; ++j)
 				matrix[i][j] += koef * matrix[k][j];
 		}
 	}
 
-	end = std::chrono::system_clock::now();
-	int elapsed_seconds = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-	std::cout << "Время работы прямого хода: " << elapsed_seconds << " микросекунд.\n";
+	elapsedtime = timeGetTime() - starttime;
+	std::cout << "Время работы прямого хода: " << elapsedtime << " миллисекундах.\n";
 
 	// обратный ход метода Гаусса
 	result[rows - 1] = matrix[rows - 1][rows] / matrix[rows - 1][rows - 1];
-	for (k = rows - 2; k >= 0; --k)
+	for (int k = rows - 2; k >= 0; --k)
 	{
 		result[k] = matrix[k][rows];
 		//
+		cilk::reducer_opadd<double> summ(0.0);
 		cilk_for (int j = k + 1; j < rows; ++j)
 		{
-			result[k] -= matrix[k][j] * result[j];
+			summ += matrix[k][j] * result[j];
 		}
+		result[k] -= summ.get_value();
 		result[k] /= matrix[k][k];
 	}
+	return elapsedtime;
 }
 
 int main()
 {
+	int consistent = 0;
+	int parallel = 0;
+
 	setlocale(LC_ALL, "Russian");
 	srand((unsigned)time(0));
 
 	int i;
 	// кол-во строк в матрице, приводимой в качестве примера
-	//const int test_matrix_lines = 4;
+	/*const int test_matrix_lines = 4;
 
-	//double **test_matrix = new double*[test_matrix_lines];
+	double **test_matrix = new double*[test_matrix_lines];
 
 	// (test_matrix_lines+1) - количество столбцов в тестовой матрице,
 	// последний столбец матрицы отведен под правые части уравнений, входящих в СЛАУ
-	/*for (i = 0; i < (test_matrix_lines + 1); ++i)
+	for (i = 0; i < (test_matrix_lines + 1); ++i)
 	{
 		test_matrix[i] = new double[(test_matrix_lines + 1)];
-	}*/
+	}
 
 	// массив решений СЛАУ
-	//double *result = new double[test_matrix_lines];
+	double *result = new double[test_matrix_lines];
 
 	// инициализация тестовой матрицы
-	//test_matrix[0][0] = 2; test_matrix[0][1] = 5;  test_matrix[0][2] = 4;  test_matrix[0][3] = 1;  test_matrix[0][4] = 20;
-	//test_matrix[1][0] = 1; test_matrix[1][1] = 3;  test_matrix[1][2] = 2;  test_matrix[1][3] = 1;  test_matrix[1][4] = 11;
-	//test_matrix[2][0] = 2; test_matrix[2][1] = 10; test_matrix[2][2] = 9;  test_matrix[2][3] = 7;  test_matrix[2][4] = 40;
-	//test_matrix[3][0] = 3; test_matrix[3][1] = 8;  test_matrix[3][2] = 9;  test_matrix[3][3] = 2;  test_matrix[3][4] = 37;
+	test_matrix[0][0] = 2; test_matrix[0][1] = 5;  test_matrix[0][2] = 4;  test_matrix[0][3] = 1;  test_matrix[0][4] = 20;
+	test_matrix[1][0] = 1; test_matrix[1][1] = 3;  test_matrix[1][2] = 2;  test_matrix[1][3] = 1;  test_matrix[1][4] = 11;
+	test_matrix[2][0] = 2; test_matrix[2][1] = 10; test_matrix[2][2] = 9;  test_matrix[2][3] = 7;  test_matrix[2][4] = 40;
+	test_matrix[3][0] = 3; test_matrix[3][1] = 8;  test_matrix[3][2] = 9;  test_matrix[3][3] = 2;  test_matrix[3][4] = 37;
+
+	//consistent = SerialGaussMethod(test_matrix, test_matrix_lines, result);
+	parallel = SerialGaussParallelMethod(test_matrix, test_matrix_lines, result);
+	std::cout << "Ускорение: " << (double)consistent / parallel << ".\n";
+
+	for (i = 0; i < test_matrix_lines; ++i)
+	{
+		delete[]test_matrix[i];
+	}
+
+	printf("Solution:\n");
+
+	for (i = 0; i < test_matrix_lines; ++i)
+	{
+		printf("x(%d) = %lf\n", i, result[i]);
+	}
+
+	delete[] result;*/
 
 	double **matrix = new double*[MATRIX_SIZE];
 	for (i = 0; i < (MATRIX_SIZE + 1); ++i)
@@ -152,7 +172,8 @@ int main()
 
 	IntiMatrix(matrix);
 	
-	SerialGaussMethod(matrix, MATRIX_SIZE, result);
+	consistent = SerialGaussMethod(matrix, MATRIX_SIZE, result);
+	parallel = SerialGaussParallelMethod(matrix, MATRIX_SIZE, result);
 
 	printf("\nSolution:\n");
 	for (i = 0; i < MATRIX_SIZE; ++i)
@@ -163,6 +184,8 @@ int main()
 
 	delete[] result;
 
+	if ((consistent > 0) && (parallel > 0))
+	std::cout << "Ускорение: " << (double)consistent / parallel << ".\n";
 	system("pause");
 
 	return 0;
